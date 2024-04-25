a85992041c5579ed998535e1e44102fd8c065e41
Harry MacCode <emarh.harry.code@gmail.com>
2024-04-26 00:57:00 +0200

    Delete a Section

--- controllers/courseSection.controller.js
+++ controllers/courseSection.controller.js
@@ -4,6 +4,7 @@ const User = require('../models/user.model');
 const CourseSection = require('../models/courseSection.model');
 const Course = require('../models/course.model');
 
+/************* Create a Section *************/
 const create = asyncHandler(async (req, res) => {
     //!Find the user
     const userFound = await User.findById(req.user);
@@ -50,11 +51,13 @@ const create = asyncHandler(async (req, res) => {
     });
 });
 
+/************* Get All Sections *************/
 const getAllSections = asyncHandler(async(req, res) => {
     const courseSections = await CourseSection.find();
     res.json(courseSections);
 });
 
+/************* Get a Section By ID *************/
 const getSectionByID = asyncHandler(async(req, res) => {
     const { sectionID } = req.params;
     // console.log(sectionID);
@@ -68,6 +71,7 @@ const getSectionByID = asyncHandler(async(req, res) => {
     }
 });
 
+/************* Update a Section *************/
 const updateASection = asyncHandler(async(req, res) => {
     const { sectionID } = req.params;
     // const courseSection = await CourseSection.findById(sectionID);
@@ -81,4 +85,37 @@ const updateASection = asyncHandler(async(req, res) => {
     }
 });
 
-module.exports = { create, getAllSections, getSectionByID, updateASection };
+/************* Delete a Section *************/
+const deleteASection = asyncHandler(async(req, res) => {
+    const { sectionID } = req.params;
+    //! Find the section to be deleted
+    const sectionFound = await CourseSection.findById(sectionID);
+
+    if (!sectionFound) {
+        res.send(404);
+        res.json({ message: 'Section not found' });
+        return;
+    }
+
+    //!Find the course associated with the section to check for enrolled students
+    const course = await Course.findOne({ sections: req.params.sectionID }).populate("students");
+    console.log('course', course);
+    if (!course) {
+    res.status(404).json({ message: "Associted course not found" });
+    }
+    //! Check if the course has any students enrolled
+    if (course.students.length > 0) {
+        res.status(400).json({ message: "Associted course has students, cannot delete section" });
+        return;
+    }
+
+    //!Proceed to delete
+    await CourseSection.findByIdAndDelete(sectionID);
+    //! Remove the section from the course's sections array
+    await Course.findByIdAndUpdate(course._id, {
+      $pull: { sections: sectionID },
+    });
+    res.json({ message: "Section deleted successfully" });
+});
+
+module.exports = { create, getAllSections, getSectionByID, updateASection, deleteASection };
--- routes/courseSection.route.js
+++ routes/courseSection.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const isAuthenticated = require('../middlewares/isAuth.middleware');
-const { create, getAllSections, getSectionByID, updateASection } = require('../controllers/courseSection.controller');
+const { create, getAllSections, getSectionByID, updateASection, deleteASection } = require('../controllers/courseSection.controller');
 const router = express.Router();
 
 router.post("/create/:courseId", isAuthenticated, create);
 router.get("/", isAuthenticated, getAllSections);
 router.get("/:sectionID", isAuthenticated, getSectionByID);
 router.put("/:sectionID", isAuthenticated, updateASection);
+router.delete("/:sectionID", isAuthenticated, deleteASection);
 
 module.exports = router;
